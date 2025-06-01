d9db4c67d0b117cc041dd2eff0aed4a680adbad8
Daniel <danieloliv.contato@gmail.com>
2025-06-01 11:35:39 -0300

    fix: ajustes validação ProdutoDto

--- src/back/bioce/src/model/produto/produto.dto.ts
+++ src/back/bioce/src/model/produto/produto.dto.ts
@@ -1,15 +1,19 @@
-import {IsNotEmpty} from "class-validator";
+import {IsDateString, IsNotEmpty, IsNumber, IsString} from "class-validator";
 
 export class ProdutoDto {
     @IsNotEmpty()
+    @IsString()
     nome: string
 
+    @IsDateString()
     @IsNotEmpty()
     dataValidade: Date;
 
+    @IsDateString()
     @IsNotEmpty()
     dataFab: Date
 
     @IsNotEmpty()
+    @IsNumber()
     usuarioId: number;
 }
