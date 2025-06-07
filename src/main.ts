7d8e4197703e6f473c33f6c98310c59af18372db
Luiz Almeida <92050300+LuizAlmeida20@users.noreply.github.com>
2025-06-07 16:32:13 -0300

    env names

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(Number(process.env.PORT));
 }
 bootstrap();
