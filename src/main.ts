00b880ac87da19de4e8352068e739fb8fa03e98a
Humberto Jr <91671380+HumbertoJrGH@users.noreply.github.com>
2025-06-08 19:56:12 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import 'dotenv/config';
-import * as process from 'node:process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: 'GET, HEAD, OPTIONS, POST, PUT, PATCH, DELETE',
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+      credentials: true
+    }
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(Number(process.env.PORT));
+  await app.listen(3000);
 }
 bootstrap();
