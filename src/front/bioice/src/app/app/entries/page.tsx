e1714ef309724ad2e37801efd56fcf2f20692b4b
Humberto Jr <91671380+HumbertoJrGH@users.noreply.github.com>
2025-06-09 14:36:46 -0300

    correções

--- src/front/bioice/src/app/app/entries/page.tsx
+++ src/front/bioice/src/app/app/entries/page.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from "react"
 import RowLancamento, { RowData } from "@/components/Sheets/Entry"
 import Button from "@/components/basic/Button"
 import Card from "@/components/basic/Card"
+import MoneyInput from "@/components/basic/MoneyInput"
 
 const tabs = ["Entradas", "Saídas", "Insumos", "Relatórios", "Admin"]
 
@@ -195,40 +196,4 @@ function ModalAddEntry({ onClose, onFinish }: ModalAddEntry) {
 			</form>
 		</div>
 	</div>
-}
-
-interface MoneyInput {
-	name: string
-	value: string | number
-	onChange?: (e?: React.ChangeEvent<HTMLInputElement>) => void
-}
-
-export function MoneyInput({ name, value, onChange }: MoneyInput) {
-	// Formata o valor em centavos para R$ X,XX
-	const formatted = new Intl.NumberFormat("pt-BR", {
-		style: "currency",
-		currency: "BRL",
-	}).format(Number(value || "0") / 100);
-
-	const handleLocalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const onlyNums = e.target.value.replace(/\D/g, "");
-		const syntheticEvent = {
-			...e,
-			target: {
-				...e.target,
-				name,
-				value: onlyNums,
-			},
-		};
-		if (onChange) onChange(syntheticEvent as React.ChangeEvent<HTMLInputElement>);
-	};
-
-	return (
-		<input
-			name={name}
-			value={formatted}
-			onChange={handleLocalChange}
-			className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm text-gray-800 focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-600"
-		/>
-	);
 }
