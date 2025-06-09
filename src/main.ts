b68c9e9a5daac4cd5b526e337fe18c3fb6c71f51
Luiz Almeida <92050300+LuizAlmeida20@users.noreply.github.com>
2025-06-08 23:01:05 -0300

    fix: main.ts env values

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: process.env.ORIGIN,
       methods: 'GET, HEAD, OPTIONS, POST, PUT, PATCH, DELETE',
       allowedHeaders: 'Content-Type, Accept, Authorization',
       credentials: true
     }
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
