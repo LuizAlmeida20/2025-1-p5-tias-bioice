5e921b2383274e2b0f6719508d6ef250b3c770aa
Daniel <danieloliv.contato@gmail.com>
2025-06-28 18:33:11 -0300

    refactor: verificarExistenciaDoInsumo

--- src/back/bioce/src/repository/insumo/insumo.repository.ts
+++ src/back/bioce/src/repository/insumo/insumo.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SelectQueryBuilder } from 'typeorm';
-import { PaginacaoDto } from '../../model/insumo/dto/paginacao.dto';
+import {PaginacaoDto} from "../../shared/dto/paginacao.dto";
 import {Insumo} from "../../model/insumo/insumo.entity";
 
 @Injectable()
@@ -31,14 +31,23 @@ export class InsumoRepository {
     ): Promise<boolean> {
         const { id, nome, dataValidade, dataRegistro, lote, descricao } = insumo;
 
-        const query: SelectQueryBuilder<Insumo> = Insumo.createQueryBuilder(
-            'insumo',
-        )
-            .andWhere('insumo.nome_produto = :nome', { nome })
-            .andWhere('insumo.dt_validade = :dataValidade', { dataValidade })
-            .andWhere('insumo.Dt_registro = :dataRegistro', { dataRegistro })
-            .andWhere('insumo.lote = :lote', { lote })
-            .andWhere('insumo.Descrição = :descricao', { descricao });
+        const query: SelectQueryBuilder<Insumo> = Insumo.createQueryBuilder('insumo')
+
+        if (nome) {
+            query.andWhere('insumo.nome_produto = :nome', { nome });
+        }
+        if (dataValidade) {
+            query.andWhere('insumo.dt_validade = :dataValidade', { dataValidade });
+        }
+        if (dataRegistro) {
+            query.andWhere('insumo.Dt_registro = :dataRegistro', { dataRegistro });
+        }
+        if (lote) {
+            query.andWhere('insumo.lote = :lote', { lote });
+        }
+        if (descricao) {
+            query.andWhere('insumo.Descrição = :descricao', { descricao });
+        }
         if (id) {
             query.andWhere('insumo.id != :id', { id });
         }
