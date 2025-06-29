2760fb5fe19b88c00a7a36e3c6db4da1ee876abc
Luiz Almeida <92050300+LuizAlmeida20@users.noreply.github.com>
2025-06-29 08:20:59 -0300

    fix: repo recover

--- src/front/bioice/src/components/basic/Card.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react"
-
-export default function Card(props: { children: ReactNode }) {
-	return <div className="bg-white rounded-2xl shadow-lg p-6 space-y-6">
-		{props.children}
-	</div>
-}
